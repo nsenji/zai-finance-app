82877f12184675d65d50dc1467b5da57f7efbc12
Ian Wasukira <ianwasukira@gmail.com>
2023-11-19 15:36:23 +0300

    Created the models upon which the database is based on

---
+++ lib/features/authentication/domain/request.dart
@@ -0,0 +1,7 @@
+class Request {
+  int requestorId;
+  int requesteeId;
+  String reason;
+
+  Request(this.requestorId, this.requesteeId, this.reason);
+}
---
+++ lib/features/authentication/domain/user.dart
@@ -0,0 +1,9 @@
+class User {
+  String name;
+  String number;
+  String email;
+  String username;
+  int balance;
+  
+  User(this.name, this.number, this.email, this.username, this.balance);
+}
