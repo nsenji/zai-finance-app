85e586eb7786e194f1886539c274519d2f653a55
nsenji <jerrysyre@gmail.com>
2023-12-26 15:50:57 +0300

    the layout for both the analytics is done

--- lib/features/analytics/presentation/analytics_screen.dart
+++ lib/features/analytics/presentation/analytics_screen.dart
@@ -16,119 +16,169 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Color.fromARGB(255, 240, 240, 240),
+        child: DefaultTabController(
+          length: 2,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Balance",
-                          style: TextStyle(
-                              color: const Color.fromARGB(255, 92, 92, 92),
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "UGX 2399000",
-                          style: TextStyle(
-                              color: Colors.green,
-                              fontSize: 19,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8),
-                      child: Row(
-                        children: [
-                          Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.black12),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: SvgPicture.asset(
-                                  "assets/images/expenses.svg",
-                                  color: Colors.red,
-                                ),
-                              )),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.black12),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: SvgPicture.asset(
-                                  "assets/images/income.svg",
-                                  color: Colors.green,
-                                ),
-                              ))
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: Container(
+              Container(
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Color.fromARGB(255, 255, 243, 243)),
+                  borderRadius: BorderRadius.circular(8),
+                  color: Color.fromARGB(255, 240, 240, 240),
+                ),
                 child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Expenditure",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Color.fromARGB(255, 112, 112, 112)),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              "-UGX 239045",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.red),
-                            )
-                          ],
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Balance",
+                            style: TextStyle(
+                                color: const Color.fromARGB(255, 92, 92, 92),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "UGX 2399000",
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-                      AnalyticsTest(),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child:
+                        
+                          ButtonsTabBar(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle
+                            ),
+                            tabs: [
+                            Tab(
+                              child: Container(
+                                  // decoration: BoxDecoration(
+                                  //     shape: BoxShape.circle,
+                                  //     color: Colors.black12),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SvgPicture.asset(
+                                      "assets/images/expenses.svg",
+                                      color: Colors.red,
+                                    ),
+                                  )),
+                            ),
+                            
+                            Tab(
+                              child: Container(
+                                  // decoration: BoxDecoration(
+                                  //     shape: BoxShape.circle,
+                                  //     color: Colors.black12),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SvgPicture.asset(
+                                      "assets/images/income.svg",
+                                      color: Colors.green,
+                                    ),
+                                  )),
+                            ),
+                          ]),
+                        
+                      )
                     ],
                   ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: TabBarView(
+                  children:[ Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Color.fromARGB(255, 255, 243, 243)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Expenditure",
+                                  style: TextStyle(
+                                      color: const Color.fromARGB(255, 92, 92, 92),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  "-UGX 239045",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.red),
+                                )
+                              ],
+                            ),
+                          ),
+                          AnalyticsTest(),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Color.fromARGB(255, 255, 243, 243)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Expenditure",
+                                  style: TextStyle(
+                                      color: const Color.fromARGB(255, 92, 92, 92),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  "-UGX 239045",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.red),
+                                )
+                              ],
+                            ),
+                          ),
+                          AnalyticsTest(),
+                        ],
+                      ),
+                    ),
+                  ),
+            ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
