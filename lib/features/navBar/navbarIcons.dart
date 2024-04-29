f979bbb90a835f4ce3d0f2879ba15c99b96bf51c
victor-nsengiyumva <jerrysyre@gmail.com>
2024-04-29 22:26:37 +0300

    made changes to deprecated files

--- lib/features/navBar/navbarIcons.dart
+++ lib/features/navBar/navbarIcons.dart
@@ -34,16 +34,6 @@ class _BarIconState extends State<BarIcon> {
               : const Color(0xFFA9A8AA),
         );
 
-      // case 'Budget':
-      //   return SvgPicture.asset(
-      //     'assets/images/budget.svg',
-      //     height: 23,
-      //     width: 23,
-      //     color: widget.active
-      //         ? Colors.black
-      //         : const Color(0xFFA9A8AA),
-      //   );
-
       case 'Transactions':
         return SvgPicture.asset(
           'assets/images/transactions.svg',
